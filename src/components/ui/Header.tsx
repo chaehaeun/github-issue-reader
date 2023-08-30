a0191614fd3cf61fa18b566c8d35f6d1bfbbc642
chaehaeun <chae_3210@naver.com>
2023-08-30 16:04:02 +0900

    [Feat] 홈으로 이동 버튼 추가

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,11 +1,36 @@
+import { useLocation, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 const ORGNAME = "facebook";
 const REPONAME = "react";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const goBack = () => {
+    navigate("/");
+  };
+
   return (
     <StyledHeader>
+      {location.pathname !== "/" && (
+        <BackButton type="button" aria-label="뒤로가기" onClick={goBack}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3"
+            />
+          </svg>
+        </BackButton>
+      )}
       {ORGNAME} / {REPONAME}
     </StyledHeader>
   );
@@ -17,6 +42,21 @@ const StyledHeader = styled.header`
   font-weight: 600;
   padding: 1rem;
   border-bottom: 1px solid #393939;
+  position: relative;
+`;
+
+const BackButton = styled.button`
+  position: absolute;
+  top: 1rem;
+  left: 1rem;
+  cursor: pointer;
+  color: #000;
+  background-color: transparent;
+  border: none;
+  svg {
+    width: 1.5rem;
+    height: 1.5rem;
+  }
 `;
 
 export default Header;
