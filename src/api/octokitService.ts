7b7b0b7ce3c2fe75fbfadcd6efa0d7f9d640849a
chaehaeun <chae_3210@naver.com>
2023-08-30 10:47:12 +0900

    [Feat] api axios에서 octokit으로 변경

---
+++ src/api/octokitService.ts
@@ -0,0 +1,49 @@
+import { Octokit } from "@octokit/rest";
+
+const octokit = new Octokit({
+  auth: process.env.REACT_APP_GITHUB_ACCESS_TOKEN,
+});
+
+export const getIssues = async (owner = "facebook", repo = "react") => {
+  try {
+    const response = await octokit.rest.issues.listForRepo({
+      owner,
+      repo,
+      state: "open",
+      sort: "comments",
+    });
+
+    const data = response.data.map((issue) => ({
+      id: issue.id,
+      title: issue.title,
+      number: issue.number,
+      author: issue.user?.login,
+      comments: issue.comments,
+      createdAt: issue.created_at,
+    }));
+
+    console.log(response.data);
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const getIssueDetail = async (
+  issueNumber: number,
+  owner = "facebook",
+  repo = "react"
+) => {
+  try {
+    const response = await octokit.rest.issues.get({
+      owner,
+      repo,
+      issue_number: issueNumber,
+    });
+    return response.data;
+  } catch (error) {
+    console.error(`#${issueNumber}:`, error);
+    throw error;
+  }
+};
