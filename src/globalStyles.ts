2daaa9c4f694a43b990c947a0174991f598c120a
chaehaeun <chae_3210@naver.com>
2023-08-30 13:47:04 +0900

    [Design] 글로벌 스타일 추가

---
+++ src/globalStyles.ts
@@ -0,0 +1,46 @@
+import { createGlobalStyle, css } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  a{
+        text-decoration: none;
+        color: inherit;
+    }
+    *{
+        box-sizing: border-box;
+    }
+    html, body, div, span, h1, h2, h3, h4, h5, h6, p, 
+    a, dl, dt, dd, ol, ul, li, form, label, table{
+        margin: 0;
+        padding: 0;
+        border: 0;
+    }
+    body{
+        line-height: 1;
+        font-family: 'Noto Sans KR', sans-serif;
+        margin-bottom: 100px;
+    }
+    ol, ul{
+        list-style: none;
+    }
+    button {
+        border: 0;
+        background: transparent;
+        cursor: pointer;
+    }
+    #root{
+        max-width: 1200px;
+        margin: 0 auto;
+    }
+    `;
+
+export const a11yHidden = css`
+  overflow: hidden;
+  position: absolute !important;
+  clip: rect(0, 0, 0, 0);
+  clip-path: inset(50%);
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+`;
+
+export default GlobalStyles;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-// import { router } from "@/router";
 import router from "./router/router";
+import GlobalStyles from "globalStyles";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <>
+    <GlobalStyles />
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </>
+  // </React.StrictMode>
 );
