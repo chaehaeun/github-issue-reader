2bd66fb4c88f5b1c9c205d9f0179b12922e926bf
chaehaeun <chae_3210@naver.com>
2023-09-01 11:47:03 +0900

    [Style] 가독성 향상을 위해 스위치문에서 if문으로 변경

--- src/api/octokitService.ts
+++ src/api/octokitService.ts
@@ -41,13 +41,14 @@ export const getIssueDetail = async (issueNumber: string) => {
     issue_number,
   });
 
-  switch (true) {
-    case response.status !== 200:
-      throw new Error("해당 이슈를 불러올 수 없거나 잘못된 접근입니다.");
-    case !!response.data.pull_request:
-      throw new Error("해당 페이지는 이슈가 아닙니다.");
-    case response.data.state !== "open":
-      throw new Error("해당 이슈는 닫혀있습니다.");
+  if (response.status !== 200) {
+    throw new Error("이슈를 불러오는데 실패했습니다.");
+  }
+  if (response.data.pull_request) {
+    throw new Error("해당 페이지는 이슈가 아닙니다.");
+  }
+  if (response.data.state !== "open") {
+    throw new Error("해당 이슈는 닫혀있습니다.");
   }
 
   const data = {
